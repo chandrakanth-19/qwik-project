0c2c8853c29eb310648652875fc00938433c9eb0
chandrakanth-19 <chandrark57@gmail.com>
2026-03-24 19:33:35 +0530

    Re-deploying the frontend on vercel

--- qwik-client/src/api/axios.js
+++ qwik-client/src/api/axios.js
@@ -31,7 +31,12 @@
 import axios from "axios";
 import toast from "react-hot-toast";
 
-const api = axios.create({ baseURL: "/api" });
+//const api = axios.create({ baseURL: "/api" });
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_URL
+    ? `${import.meta.env.VITE_API_URL}/api`
+    : "/api",
+});
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("qwik_token");
