6c50479084b77280f592336ccfb07d01175f98e0
beluga <chandrark57@gmail.com>
2026-04-12 17:23:14 +0530

    Fix formatting in Payment.model.js

--- qwik-server/src/models/Payment.model.js
+++ qwik-server/src/models/Payment.model.js
@@ -7,7 +7,7 @@ const paymentSchema = new mongoose.Schema(
     user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     amount: { type: Number, required: true },
     method: { type: String, enum: Object.values(PAYMENT_METHOD) },
-    gateway_txn_id: { type: String, default: "" }, // RazorPay txn id
+    gateway_txn_id: { type: String, default: "" }, 
     razorpay_order_id: { type: String, default: "" },
     status: {
       type: String,
